db36f99f2ebded83f70817eb0fea26156499b270
Khurram Shahzad <khshahza@login-20-26.hpcc.ttu.edu>
2026-07-04 13:37:27 -0500

    Module 1: Core pipeline infrastructure added

---
+++ config/config.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+############################
+# Comparative Genome Pipeline
+# Config file (USER EDIT ONLY THIS FILE)
+############################
+
+PROJECT="Chlorella"
+
+THREADS=64
+MEMORY="250G"
+
+# Paths
+PROJECT_DIR=$PWD
+DATA_DIR=$PROJECT_DIR/data
+RESULTS_DIR=$PROJECT_DIR/results
+LOGS_DIR=$PROJECT_DIR/logs
+
+# Genomes
+GENOMES=(
+"utex2526"
+"utex395"
+"utexb3016"
+)
+
+# Files
+FASTA_EXT=".fasta"
+PROTEIN_EXT=".faa"
+GFF_EXT=".gff3"
+
+# Tools
+MCSCANX="/home/khshahza/MCScanX/MCScanX"
+BLAST_THREADS=64
+
+echo "Config loaded for project: $PROJECT"
